d14313f6b1e89086fdf923d73af3d009a21cb6f4
Jeff Brock <boccobrock@gmail.com>
2016-11-10 23:03:48 -0700

    some improvements

--- boat.rb
+++ boat.rb
@@ -3,13 +3,15 @@ require 'socket'
 abort "Usage - ruby boat.rb host port" if ARGV.empty?
 
 client = TCPSocket.new ARGV[0], ARGV[1]
-board = "AAAAABBBB." + ("." * 10 * 8) + "CCCDDDEE.."
-shots = Array.new(100) { |x| (x % 10 + 1).to_s + "|" + (x / 10 + 1).to_s }
+board = "AAAAA.BBBB" + ("." * 10 * 8) + "CCC.DDD.EE"
+shots = Array.new(100) { |x| {:x => x % 10 + 1, :y => x / 10 + 1} }
 shots.shuffle!
 
 while message = client.gets
-    puts message
     if message[0] == "G" then client.puts "J|McBoatFace|#{board}" end
-    if message[0] == "H" then client.puts "M|rando|ouch!" end
-    if message.start_with? "N|McBoatFace" then client.puts "S|rando|" + shots.pop end
+    if message[0] == "J" && message != "J|McBoatFace" then enemy = message.strip.sub /J\|/, ''  end
+    if message.start_with? "N|McBoatFace" then client.puts "S|#{enemy}|#{shots.first[:x]}|#{shots.shift[:y]}" end
+    if message.start_with? "H|McBoatFace" then lasthit = message end
+#shots.sort_by! { |a| (3 - a[:x]).abs + (3 - a[:y]).abs }
+    if message.start_with? "R|McBoatFace" then puts message end
 end
