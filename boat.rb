21989fea1d66d6381b9758a4e75441f8eace8db7
Jeff Brock <boccobrock@gmail.com>
2016-11-11 11:22:57 -0700

    Update boat.rb

--- boat.rb
+++ boat.rb
@@ -11,7 +11,10 @@ while message = client.gets
     if message[0] == "G" then client.puts "J|McBoatFace|#{board}" end
     if message[0] == "J" && message != "J|McBoatFace" then enemy = message.strip.sub /J\|/, ''  end
     if message.start_with? "N|McBoatFace" then client.puts "S|#{enemy}|#{shots.first[:x]}|#{shots.shift[:y]}" end
-    if message.start_with? "H|McBoatFace" then lasthit = message end
-#shots.sort_by! { |a| (3 - a[:x]).abs + (3 - a[:y]).abs }
+    if message.start_with? "H|McBoatFace" then
+        x = (message.split('|')[3][1..5].ord - 96).to_i
+        y = (message.split('|')[3][1..2]).to_i
+        shots.sort_by! { |a| (x - a[:x]).abs + (y - a[:y]).abs }
+    end
     if message.start_with? "R|McBoatFace" then puts message end
 end
